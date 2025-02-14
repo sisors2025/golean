8bda52b8b8a0ee70f004933e85654abe30674de7
AndresCorral <andresco70@hotmail.com>
2025-02-14 12:57:43 -0500

    testing robot.txt

--- app/robots.ts
+++ app/robots.ts
@@ -1,20 +1,20 @@
-import { MetadataRoute } from 'next';
+import { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   const domain = process.env.NEXT_PUBLIC_DOMAIN;
 
   return {
     rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: [
-        '/api/',
-        '/admin/',
-        '/_next/',
-        '/static/',
-        '/*.json$',
-        '/*.xml$',
-      ],
+      userAgent: "*",
+      allow: "/",
+      // disallow: [
+      //   '/api/',
+      //   '/admin/',
+      //   '/_next/',
+      //   '/static/',
+      //   '/*.json$',
+      //   '/*.xml$',
+      // ],
     },
     sitemap: `${domain}/sitemap.xml`,
     host: domain,
